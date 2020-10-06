24016e2b1c3e5cd91de37b682d550c942f3b1c74
John <jfurcht21@gmail.com>
2020-10-05 20:33:04 -0700

    Updates binary search logic.

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -7,19 +7,29 @@ public class Problem1 {
         int low = 0;
         int high = data.length - 1;
 
+        //Performs checks to qualify the array before it enters the for loop.
+        //If the following conditions are met, these preset values are returned
+        if (data == null || data.length == 0) {
+            return -1;
+        }
+        if (data.length == 1 && target != data[0]) {
+            return -1;
+        }
+        if (target == data[0]) {
+            return 0;
+        }
+
         //Checks the middle number of the array to see if the requested number matches.
         //Performs binary search until number is found.
         for (int i = 0; i < data.length; i++) {
             if (target == data[middleIndex])
                 return middleIndex;
             if (target > data[middleIndex]) {
-                int increaseMiddleBy = (high - middleIndex) / 2;
-                low = middleIndex;
-                middleIndex = middleIndex + increaseMiddleBy;
+                low = middleIndex + 1;
+                middleIndex = low;
             } else {
-                int decreaseMiddleBy = (middleIndex - low) / 2;
-                high = middleIndex;
-                middleIndex = middleIndex - decreaseMiddleBy;
+                high = middleIndex - 1;
+                middleIndex = high;
             }
         }
         return -1; // Place holder
