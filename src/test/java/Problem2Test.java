17d24541bb90e2e784763c740081a4be3fec9f8b
John <jfurcht21@gmail.com>
2020-10-06 12:39:44 -0700

    Updates bubble sort test.

--- src/test/java/Problem2Test.java
+++ src/test/java/Problem2Test.java
@@ -8,7 +8,24 @@ public class Problem2Test {
     // Okay to change
     @Test
     public void testBubbleSort() {
-        assertTrue(false);
-        // add your tests here
+
+        int[] input = new int[] {3, 2, 5, 6, 4, 10, 22, 9};
+        int[] input2 = new int[] {3, 2, 5};
+        int[] input3 = new int[] {3,1};
+
+        Problem2.bubbleSort(input);
+        for (int i = 0; i < input.length-1; i++){
+            assertTrue(input[i]<input[i+1]);
+        }
+
+        Problem2.bubbleSort(input2);
+        for (int i = 0; i < input2.length-1; i++){
+            assertTrue(input2[i]<input2[i+1]);
+        }
+
+        Problem2.bubbleSort(input3);
+        for (int i = 0; i < input3.length-1; i++){
+            assertTrue(input3[i]<input3[i+1]);
+        }
     }
 }
